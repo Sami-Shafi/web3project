325711172457204f713dff7f5770582c07235f4e
sami-shafi <freelancersami123@gmail.com>
2023-01-02 04:48:20 +0600

    fund me payable 1

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,26 +4,53 @@
 
 
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.8;
+pragma solidity ^0.8.7;
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    function fund() public payable  {
-        // set minimum fund in USD
-        require(msg.value >= 1e18, "Value is less than minimum amount.");
+    /*
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address[] public funders;
+    address public owner;
+
+    constructor()  {
+        owner = msg.sender;
+    }
+    */
+
+    function fund() public payable {
+        uint256 minimumUSD = 50 * 1e18;
+        require(
+            conversion(msg.value) >= minimumUSD,
+            "You need to spend more ETH!"
+        );
+        /*addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);*/
     }
 
-    function getPrice() public {
+    function getPrice() public view returns(uint256) {
         //Required: ABI, Address
         //Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+         ( ,int256 answer, , , ) = priceFeed.latestRoundData();
+         // price: 120186000000
+
+         return uint256(answer * 1e10);
+    }
+
+    function conversion(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountToUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountToUsd;
     }
 
     function getVersion() public view returns(uint256) {
-        AggregatorV3Interface priceFeed =  AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
         return priceFeed.version();
     }
 
-    function getConversionRate() public {}
-
-    function withdraw() public payable {}
+    // function withdraw() public payable {}
 }
