6c894487a16d9f99eced27ca5454d5f0e316cc5f
sami-shafi <freelancersami123@gmail.com>
2023-01-01 19:49:20 +0600

    fundme code start

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -8,10 +8,17 @@ contract StorageFactory {
     SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
-
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
     }
 
-    function sfStore()
+    function storeFunc(uint256 _itemIndex, uint256 _itemParamValue) public {
+        //required: address, ABI
+        simpleStorageArray[_itemIndex].store(_itemParamValue);
+    }
+
+    function retrieveFunc(uint256 _itemIndex) public view returns(uint256) {
+        return simpleStorageArray[_itemIndex].retrieve();
+    }
 
 }
