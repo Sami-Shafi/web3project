eb9295f3b9fcf8f5a825b0fc9c790fb06afd7bdf
sami-shafi <freelancersami123@gmail.com>
2023-01-03 02:35:29 +0600

    fallback functions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -26,6 +26,14 @@ contract FundMe {
         i_owner = msg.sender;
     }
 
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
     function fund() public payable {
         if (msg.value.conversion() < MINIMUM_USD) revert MinimumValueError();
 
